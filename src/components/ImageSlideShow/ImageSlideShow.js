c101436012ccca146b3ad658d2ebf71fa1b598ef
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-10-10 17:17:25 +0530

    Build the Image Slider

--- src/components/ImageSlideShow/ImageSlideShow.js
+++ src/components/ImageSlideShow/ImageSlideShow.js
@@ -1,23 +1,46 @@
 // import { useState } from "react";
 // import data from "../ImageSlideShow/data";
+import SimpleImageSlider from "react-simple-image-slider";
 import "../ImageSlideShow/ImageSlideshow.css";
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/306381/pexels-photo-306381.jpeg?cs=srgb&dl=pexels-salah-alawadhi-306381.jpg&fm=jpg",
+  },
+  {
+    url: "https://images.pexels.com/photos/833773/pexels-photo-833773.jpeg?cs=srgb&dl=pexels-andrew-jensen-833773.jpg&fm=jpg",
+  },
+  {
+    url: "https://images.pexels.com/photos/394545/pexels-photo-394545.jpeg?cs=srgb&dl=pexels-baurzhan-kadylzhanov-394545.jpg&fm=jpg",
+  },
+  {
+    url: "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    url: "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  },
+  {
+    url: "https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    url: "https://images.pexels.com/photos/92866/pexels-photo-92866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
 
 const ImageSlideshow = () => {
   return (
     <div className="slideshow-container">
-      {/* <div className="slide-image slide-image-one"></div> */}
-      <div className="slide-image slide-image-two"></div>
-      {/* <div className="slide-image slide-image-three"></div> */}
+      <SimpleImageSlider
+        width={800}
+        height={500}
+        images={images}
+        showBullets={true}
+        showNavs={false}
+        autoPlay={true}
+        slideDuration={2}
+        loop
+      />
     </div>
-    // <div className="slideshow-container">
-    //   {data.map((obj, index) => {
-    //     return (
-    //       <div>
-    //         <img src="" alt="" />
-    //       </div>
-    //     );
-    //   })}
-    // </div>
   );
 };
 
